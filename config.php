27c3a166392863b33973011c59982b12a371857e
Olya <97823692+WinterSakuraa@users.noreply.github.com>
2022-10-15 23:20:04 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 <?php
 
 // Database config
-const DB_HOST = 'localhost';
-const DB_USER = 'wintersakura';
-const DB_PASS = '';
-const DB_NAME = 'php_conference';
+const DB_HOST = 'YOUR_HOST';
+const DB_USER = 'YOUR_USERNAME';
+const DB_PASS = 'YOUR_PASSWORD';
+const DB_NAME = 'DB_NAME';
 
 // Route config
-define("BASE_ROUTE", 'http://' . $_SERVER['HTTP_HOST'] . '/BWT_Test');
+define("BASE_ROUTE", 'YOUR_PATH');
 
 $actual_link = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 if ($actual_link == BASE_ROUTE . '/config.php') die ("Direct access not permitted");
